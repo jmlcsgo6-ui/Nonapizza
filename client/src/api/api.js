640ef9ff8b55c23248c4bafc0cc5705b679558ae
jmlcsgo6-ui <jmlcsgo6@gmail.com>
2026-04-08 20:08:08 -0300

    faa

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 // Usamos a URL do servidor que você acabou de criar no Vercel
 const API_URL = import.meta.env.VITE_API_URL || 'https://nonapizza-lk8e.vercel.app';
 
+console.log("Conectando na API:", API_URL);
+
 const api = axios.create({
     baseURL: API_URL
 });
